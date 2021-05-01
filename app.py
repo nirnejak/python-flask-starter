ec8bdeac57805fbc18c87fb451e88393834f6da7
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2021-05-02 02:25:35 +0530

    VSCode Setup, Auth Blueprint Added

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_marshmallow import Marshmallow
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+app.register_blueprint(auth.bp)
+
 ma = Marshmallow(app)
 
-
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index_page():
-    return "Hello World"
+    if request.method == 'POST':
+        return "POST: Hello World"
+    else:
+        return "GET: Hello World"
 
 
 if __name__ == '__main__':
